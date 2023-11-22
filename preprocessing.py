f40393361eb330aa8d545e1b26d46760c8662e88
fabiankuenzer <fabiankuenzer@gmail.com>
2023-11-22 16:30:54 +0100

    Refactor preprocessing functions

--- preprocessing.py
+++ preprocessing.py
@@ -9,21 +9,21 @@ nltk.download('punkt')
 
 
 def remove_stopwords(document, stopwords):
-    words = word_tokenize(document)
+    words = word_tokenize(str(document))
     filtered_words = [word for word in words if word.lower() not in stopwords]
     filtered_document = ' '.join(filtered_words)
     return filtered_document
 
 
 def lowercase_tokens(document):
-    words = word_tokenize(document)
+    words = word_tokenize(str(document))
     lowercase_words = [word.lower() for word in words]
     lowercase_document = ' '.join(lowercase_words)
     return lowercase_document
 
 
 def remove_punctuation(document):
-    words = word_tokenize(document)
+    words = word_tokenize(str(document))
     words_without_punctuation = [word for word in words if word not in list(string.punctuation)]
     document_without_punctuation = ' '.join(words_without_punctuation)
     return document_without_punctuation
@@ -32,7 +32,7 @@ def remove_punctuation(document):
 def lemmatization(document):
     stemmer = LancasterStemmer()
     lemmatizer = WordNetLemmatizer()
-    words = word_tokenize(document)
+    words = word_tokenize(str(document))
     result = list(map(lambda word: stemmer.stem(word) if word.endswith("ing") else lemmatizer.lemmatize(word), words))
     lemmatized_document = ' '.join(result)
     return lemmatized_document
