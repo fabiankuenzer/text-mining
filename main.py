45fe329df03e21f9615c1a4d634d2e1335775459
fabiankuenzer <fabiankuenzer@gmail.com>
2023-11-22 16:52:35 +0100

    Add stopwords to be removed from corpus

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ print('IMPORTING AND EXTRACTING DATA: SUCCESS')
 
 print('PREPARING DATA FOR ANALYSIS: START')
 custom_stopwords = stopwords.words('english')
-custom_stopwords.extend(["n't", "'m", "w/", "'d", "'s", "min", "00", "3rd", "4th", "49.99", "19.99", "50.00", "comcast", "mbps"])
+custom_stopwords.extend(["n't", "'m", "w/", "'d", "'s", "min", "00", "3rd", "4th", "49.99", "19.99", "50.00", "comcast", "mbps", "since", "one", "rep", "would"])
 custom_stopwords.extend([str(number) for number in range(0, 5000)])
 filtered_corpus = [remove_stopwords(document, custom_stopwords) for document in corpus]
 lowercase_corpus = [lowercase_tokens(document) for document in filtered_corpus]
